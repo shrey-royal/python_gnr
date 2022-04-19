aa997136602b6df793a3625b815e24e886340d7e
Shrey k <srkkadia@gmail.com>
2022-04-19 16:49:00 +0545

    .

--- April/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-
-# print(myString.lstrip(' '))  # Remove Left Spaces
-# print(myString.partition('.'))  # Partition it returns string in tuple
-# print(myString.replace('R', 'dp_r'))  # Replace
-# print(myString.removeprefix('R'))  # Remove Prefix
