21367b5606d738da32f223581d5fd2b13df4e9ce
shrey k <shreykadia.royal@gmail.com>
2022-06-22 10:36:56 +0530

    Dictionary methods and Programs

---
+++ June/22Jun.py
@@ -0,0 +1,30 @@
+myDict = {
+    "name": "Shrey K",
+    "age": 19,
+    "gender": "Male"
+}
+
+# myList = ["key 1", "key 2", "key 3"]
+# valueList = 0
+
+# print(myDict)
+
+# myDict.clear()
+# newDict = myDict.copy()
+# newDict = dict.fromkeys(myList, valueList)
+# print(myDict.get("gender"))
+# print(myDict["gender"])
+
+# print(myDict.items())
+# print(myDict.keys())
+# print(myDict.values())
+
+# print(myDict.pop("age")) # removes the value of specified key
+# print(myDict.popitem()) # removes last key:value pair 
+
+# myDict.setdefault("job Profile", "data scientist")
+
+# myDict.update({"alias": "jaynam"})
+# myDict["alias"] = "jaynam"
+# del(myDict)
+print(myDict)
