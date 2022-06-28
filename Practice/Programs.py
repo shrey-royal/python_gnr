819477ba26b4b6b379b37a38449481fd7d7a54b8
shrey k <shreykadia.royal@gmail.com>
2022-06-28 11:02:12 +0530

    answer of sort dictionary by value task

--- Practice/Programs.py
+++ Practice/Programs.py
@@ -24,3 +24,39 @@ print(words.get(scan_word)) if scan_word in words.keys() else print("Not Present
 '''
 1. Count the number of occurrence of each letter in word "MISSISSIPPI". Store count of every letter with the letter in a dictionary.
 '''
+
+"""
+3.  Sort the above dictionary according to the marks.
+{"s3":30, "s1":35, "s5":39, "s2":40, "s4":55}
+
+"""
+
+student = {}
+sorted_students = {}
+
+print("Please enter subject_name & marks of 10 students")
+
+for i in range(10):
+    sub_name = input(f"enter sub_name {i+1}: ")
+    marks = int(input(f"enter marks {i+1}: "))
+    student[sub_name] = marks
+
+print(student)
+
+temp = []
+for key, value in student.items():
+    temp.append(str(value) + "_" + key)
+temp.sort()
+
+print(temp)
+
+temp2 = []
+for string in temp:
+    temp2.append(string.split("_"))
+
+print(temp2)
+
+for x, y in temp2:
+    sorted_students[y] = x
+
+print(sorted_students)
