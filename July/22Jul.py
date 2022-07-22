097b7196af75dc942982c4220038dc41ab78e489
shrey k <shreykadia.royal@gmail.com>
2022-07-22 10:49:55 +0530

    OOPs (Objects and Classes)

---
+++ July/22Jul.py
@@ -0,0 +1,52 @@
+# OOPs in Python
+'''
+OOP means Object Oriented Programming
+
+Programming Language - C, C++, C#, Java, Pearl, Ruby, Python, R, GO, Swift
+
+POP - OOP
+
+POP -> Procedure Oriented Programming
+Memory...
+
+static memory
+fixed memory
+
+Dog -> Breed, color, behaviour, Food, Gender...
+
+
+OOPs includes : 
+
+Classes and Objects
+
+Inheritance
+Polymorphism
+Data Abstraction
+Encapsulation
+
+
+
+Classes-: 
+    > Collections of Objects
+    > Logical Entity
+    > Hypothetical Concept
+
+syntax-: 
+
+> a class is declared using keyword class
+
+class class_name():
+    pass
+
+
+Objects -: 
+    > A Real world Entity.
+
+
+Coding in python 
+ you must have to follow one case in all over the code
+ camelCase
+ PascalCase
+ snake_case
+
+'''
