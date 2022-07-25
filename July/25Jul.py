605a096d27354ce02e012c57f70b986d7d48d276
shrey k <shreykadia.royal@gmail.com>
2022-07-25 10:58:33 +0530

    oops theory Concepts

---
+++ July/25Jul.py
@@ -0,0 +1,14 @@
+# classes and Objects
+
+##Inheritance -> Heridity
+
+# Parent -> child
+# by using inheritance we will do code resusability
+
+#Polymorphism -> poly - many / morph - forms
+# It means to perform one task in many ways
+
+#Abstraction -> To hide Data
+# Hiding Non-important data
+
+#Encapsulation - Wrapping the data
