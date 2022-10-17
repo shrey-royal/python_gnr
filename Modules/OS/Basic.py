01a4d083a273bc228ce7a9f2ac479922251539e1
Shrey K <srkkadia@gmail.com>
2022-10-17 10:59:00 +0530

    OS Module

---
+++ Modules/OS/Basic.py
@@ -0,0 +1,50 @@
+# import os
+
+# to get the current working directory
+
+# print(os.getcwd())  # C:\Users\user\Desktop\Python\OS
+# os.chdir("C:\\Users\\Shrey Kadia\\Desktop") # to change the current working directory
+# print(os.getcwd())
+# os.mkdir("Krutarth_Foram")  # to create a directory
+# os.system("notepad.exe")  # to open a file
+# os.system("mspaint.exe")  # to open a file
+
+# to open multiple apps
+# os.system("notepad.exe & mspaint.exe")
+
+# Task :- Create a folder and open a file in it using python
+
+
+# to import the existing library we have to write import keyword followed by the name of the library.
+
+# pip -> package installer for python
+# path of the library -> C:\Users\user\AppData\Local\Programs\Python\Python37-32\Lib
+
+# pip install library_name -> to install the library
+
+# pip uninstall library_name -> to uninstall the library
+
+'''
+Header files are defined with some basic functions and classes which are used in the program.
+
+What are Modules?
+Modules are the files which contains the functions and classes which are used in the program.
+
+'''
+
+
+# task 1 :- Create a folder and open a file in it using python
+
+# import os
+
+# os.chdir("C:\\Users\\Shrey Kadia\\Desktop")
+# os.mkdir("Krutarth_Foram")
+# os.chdir("C:\\Users\\Shrey Kadia\\Desktop\\Krutarth_Foram")
+# os.system("notepad.exe")
+# line = "Hello World"
+# os.write(os.open("abcd1.txt", os.O_RDWR), line.encode())
+# os.system("abcd1.txt")
+
+
+# os.remove("abcd1.txt")  # to remove a file
+# os.rmdir("C:\\Users\\Shrey Kadia\\Desktop\\Krutarth_Foram")  # to remove a directory
---
+++ Modules/OS/Executing_CMDs.py
@@ -0,0 +1,19 @@
+# here we are executing the commands in the CMD using python
+
+# import os
+
+# os.system('cmd /k "date"')  # to get the date
+# os.system('cmd /c "time"')  # to get the time
+
+# /k -> to keep the CMD open after the execution of the command
+# /c -> to close the CMD after the execution of the command
+
+
+# os.system('cmd /k "color a & date & time"')  # to create a directory
+# print(os.getenv('path'))  # to get the path of the environment variable
+
+# print(os.get_exec_path())  # to get the path of the executable file
+
+# import os
+
+# print(os.uname())
